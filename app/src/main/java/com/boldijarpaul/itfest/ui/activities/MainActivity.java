811cd3384c41820f738d2efcf062f06d42f9716d
Boldijar Paul <paul.bv96@yahoo.com>
2015-11-07 10:36:31 +0200

    added some models

--- app/src/main/java/com/boldijarpaul/itfest/ui/activities/MainActivity.java
+++ app/src/main/java/com/boldijarpaul/itfest/ui/activities/MainActivity.java
@@ -2,6 +2,7 @@ package com.boldijarpaul.itfest.ui.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
 import com.boldijarpaul.itfest.R;
@@ -17,46 +18,21 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity implements PeopleView {
+public class MainActivity extends AppCompatActivity {
 
-    @Bind(R.id.text)
-    TextView mTextView;
-    PeoplePresenter mPeoplePresenter;
+    @Bind(R.id.activity_main_toolbar)
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        mPeoplePresenter = new PeoplePresenter(this, this);
-        mPeoplePresenter.getMovies();
+
+        setSupportActionBar(mToolbar);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setIcon(R.drawable.ic_heart_big);
     }
 
-    @OnClick(R.id.text)
-    void onClickText() {
-        mTextView.setText("loading");
-        mPeoplePresenter.getMovies();
-    }
 
-    @Override
-    public void showPeople(PersonResponse personResponse) {
-//        StringBuilder output = new StringBuilder();
-//        for (Person person : personResponse.people) {
-//            output.append("Person id: ").append(person.id).append(" Person name: ").append(person.name).append(" favorite class: ").append(person.favoriteClasses.get(0).name);
-//            output.append("\n\n\n");
-//        }
-//        mTextView.setText(output.toString());
-
-
-    }
-
-    @Override
-    public void showMovies(List<Movie> movies) {
-        String result = "";
-        for (Movie movie : movies) {
-            result += movie.name + " " + movie.releaseYear + " " + movie.genres.get(0).name+"\n\n";
-        }
-        mTextView.setText(result);
-
-    }
 }
